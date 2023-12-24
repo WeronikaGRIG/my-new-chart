8ab478ff9ff5e0a52d1b9c839e8f477516acd099
WeronikaGRIG <veronikaudot@gmail.com>
2023-12-24 21:57:39 +0100

    add new changes for my charts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Chart } from "react-google-charts";
+
+const data = [
+  ["Task", "Hours per Day"],
+  ["Работа", 9],
+  ["Спорт", 2],
+  ["Учеба", 4],
+  ["Время для семьи", 3],
+  ["Отдых, сон", 7],
+];
+
+const options = {
+  title: "Мой продуктивный день",
+  is3D: true,
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
     </div>
   );
 }
